a1fd73fd44098cd1a1f529d9579177e89cb3042e
Peter R Stanley <peterrstanley@gmail.com>
2014-04-26 17:41:43 -0400

    Initial commit for LD29. Empty file of main.lua and credits.lua

---
+++ src/credits.lua
@@ -0,0 +1,22 @@
+local credits = {}
+
+function credits.load()
+end
+
+function credits.draw()
+end
+
+function credits.update(dt)
+end
+
+function credits.keypressed(key,unicode)
+end
+
+function credits.keyreleased(key,unicode)
+end
+
+function credits.mousepressed(x,y,button)
+end
+
+function credits.mousereleased(x,y,button)
+end
---
+++ src/main.lua
@@ -0,0 +1,20 @@
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.keypressed(key,unicode)
+end
+
+function love.keyreleased(key,unicode)
+end
+
+function love.mousepressed(x,y,button)
+end
+
+function love.mousereleased(x,y,button)
+end
