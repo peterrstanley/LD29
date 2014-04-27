a58f85a8b6ecb9cb54269fb0a70560e2668aa50b
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 19:30:28 -0400

    Handling the health value of a machine,

--- src/entity/machine.lua
+++ src/entity/machine.lua
@@ -11,9 +11,12 @@ function machine.new(x,y)
   temp.keyreleased = machine.keyreleased
   temp.mousepressed = machine.mousepressed
   temp.mousereleased = machine.mousereleased
-
+  temp.inRange = machine.inRange
+  temp.increaseHealth = machine.increaseHealth
+  
   temp.x = x
   temp.y = y
+  temp.health = 0
   return temp
 end
 
@@ -22,7 +25,8 @@ function machine:load()
 end
 
 function machine:draw()
-  love.graphics.draw(self.asset,self.x,self.y)
+  love.graphics.print("Health: "..self.health,self.x,(self.y - 10))
+  love.graphics.draw(machine.asset,self.x,self.y)
 end
 
 function machine:update(dt)
@@ -43,4 +47,15 @@ end
 
 function machine:mousereleased(x,y,button)
 
+end
+
+function machine:inRange(x)
+  if (x > self.x and x < (self.x + 100)) then
+    return true
+  end
+  return false
+end
+
+function machine:increaseHealth(points)
+  self.health = (self.health+points)
 end
