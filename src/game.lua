3207413db7486ba3c9d467db4e5911b34b454eda
Peter R Stanley <peterrstanley@gmail.com>
2014-04-26 20:35:55 -0400

    Added the game file that will run the games itself and the levels.

---
+++ src/game.lua
@@ -0,0 +1,24 @@
+game = {}
+
+local level = 1
+
+function game.load()
+end
+
+function game.draw()
+end
+
+function game.update(dt)
+end
+
+function game.keypressed(key,unicode)
+end
+
+function game.keyreleased(key,unicode)
+end
+
+function game.mousepressed(x,y,button)
+end
+
+function game.mousereleased(x,y,button)
+end
