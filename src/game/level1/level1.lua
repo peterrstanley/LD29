ed0fef2ec032b1f6d0efd71422eb2df8110cc0fd
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 07:09:22 -0400

    This is athe first game level available for the game.

---
+++ src/game/level1/level1.lua
@@ -0,0 +1,30 @@
+level1 = {}
+
+function level1.load()
+
+end
+
+function level1.draw()
+  love.graphics.print("level 1",50,50)
+  love.graphics.print("This is a test",100,50)
+end
+
+function level1.update(dt)
+
+end
+
+function level1.keypressed(key,unicode)
+
+end
+
+function level1.keyreleased(key,unicode)
+
+end
+
+function level1.mousepressed(x,y,button)
+
+end
+
+function level1.mousereleased(x,y,button)
+
+end
