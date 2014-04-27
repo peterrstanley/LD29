74ab021ad5f28afa4925ad4e7b66545270497c54
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 07:37:17 -0400

    Added a return function to return to main menu with "Esc" is pressed.

--- src/credits.lua
+++ src/credits.lua
@@ -1,11 +1,12 @@
 credits = {}
-
+local ch = ""
 function credits.load()
 end
 
 function credits.draw()
   love.graphics.print("Credits",400,100)
   love.graphics.print("Developed By: Peter R Stanley",200,150)
+  love.graphics.print(ch,400,300)
 end
 
 function credits.update(dt)
@@ -15,6 +16,9 @@ function credits.keypressed(key,unicode)
 end
 
 function credits.keyreleased(key,unicode)
+  if (key == "escape") then
+    state = "main"
+  end
 end
 
 function credits.mousepressed(x,y,button)
