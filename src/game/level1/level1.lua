4b19c5b36c10f20edd5f2226b399b993e8aa1630
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 12:24:52 -0400

    Started added the player object in the level.

--- src/game/level1/level1.lua
+++ src/game/level1/level1.lua
@@ -11,18 +11,19 @@ function level1.draw()
   love.graphics.draw(level1.assets.background,0,0)
   love.graphics.print("level 1",50,50)
   love.graphics.print("This is a test",100,50)
+  game.user:draw()
 end
 
 function level1.update(dt)
-
+  game.user:update(dt)
 end
 
 function level1.keypressed(key,unicode)
-
+  game.user:keypressed(key,unicode)
 end
 
 function level1.keyreleased(key,unicode)
-
+  game.user:keyreleased(key,unicode)
 end
 
 function level1.mousepressed(x,y,button)
