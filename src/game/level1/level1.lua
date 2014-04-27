0a5fa2b6e74ca8e3137ba2c45db128aee82be224
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 19:32:15 -0400

    Added machine to the level.

--- src/game/level1/level1.lua
+++ src/game/level1/level1.lua
@@ -2,28 +2,38 @@ level1 = {}
 level1.assets = {
   background = love.graphics.newImage("game/level1/assets/level1.png")
 }
+local ch = ""
 
 function level1.load()
-
+   machine1 = machine.new(100,450)
+  machine1:load()
 end
 
 function level1.draw()
   love.graphics.draw(level1.assets.background,0,0)
-  love.graphics.print("level 1",50,50)
-  love.graphics.print("This is a test",100,50)
+  love.graphics.print("level 1 "..game.user:getRepairValue(),50,400)
+  love.graphics.print(ch,100,400)
+  machine1:draw()
   game.user:draw()
 end
 
 function level1.update(dt)
+  machine1:update(dt)
   game.user:update(dt)
 end
 
 function level1.keypressed(key,unicode)
+  machine1:keypressed(key,unicode)
   game.user:keypressed(key,unicode)
 end
 
 function level1.keyreleased(key,unicode)
+  machine1:keyreleased(key,unicode)
   game.user:keyreleased(key,unicode)
+  if (key == " " and machine1:inRange(game.user.x)) then
+    machine:increaseHealth(1)
+  end
+  ch = key
 end
 
 function level1.mousepressed(x,y,button)
