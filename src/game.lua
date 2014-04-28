a7cc1e0cb40b007f7c96a897233228beab12e2fe
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 20:46:10 -0400

    Added level 2 nd 3 to game. Also set the game to go to credits when the player wins.

--- src/game.lua
+++ src/game.lua
@@ -1,12 +1,13 @@
 require("entity/player")
 require("entity/machine")
+require("credits")
 
 game = {}
-local levels = {"1"}
-local cur_level = 1
+local levels = {"1","2","3"}
+game.cur_level = 1
 game.levels = {}
 
-game.user = player.new(20,475,1)
+game.user = player.new(20,475,20)
 
 for _,v in ipairs(levels) do
   require("game/level"..v.."/level"..v)
@@ -15,41 +16,80 @@ end
 
 function game.load()
   level1.load()
+  level2.load()
+  level3.load()
+  credits.load()
 end
 
 function game.draw()
-  love.graphics.print(cur_level,700,10)
-  if (cur_level == 1) then
+  love.graphics.print(game.cur_level,700,10)
+  if (game.cur_level == 1) then
     level1.draw()
+  elseif (game.cur_level == 2) then
+    level2.draw()
+  elseif (game.cur_level == 3) then
+    level3.draw()
+  elseif (game.cur_level == "done") then
+    credits.draw()
   end
 end
 
 function game.update(dt)
-  if (cur_level == 1) then
+  if (game.cur_level == 1) then
     level1.update(dt)
+  elseif (game.cur_level == 2) then
+    level2.update(dt)
+  elseif (game.cur_level == 3) then
+    level3.update(dt)
+  elseif (game.cur_level == "done") then
+    credits.update(dt)
   end
 end
 
 function game.keypressed(key,unicode)
-  if (cur_level == 1) then
+  if (game.cur_level == 1) then
     level1.keypressed(key,unicode)
+  elseif (game.cur_level == 2) then
+    level2.keypressed(key,unicode)
+  elseif (game.cur_level == 3) then
+    level3.keypressed(key,unicode)
+  elseif (game.cur_level == "done") then
+    credits.keypressed(key,unicode)
   end
 end
 
 function game.keyreleased(key,unicode)
-  if (cur_level == 1) then
+  if (game.cur_level == 1) then
     level1.keyreleased(key,unicode)
+  elseif (game.cur_level == 2) then
+    level2.keyreleased(key,unicode)
+  elseif (game.cur_level == 3) then
+    level3.keyreleased(key,unicode)
+  elseif (game.cur_level == "done") then
+    credits.keyreleased(key,unicode)
   end
 end
 
 function game.mousepressed(x,y,button)
-  if (cur_level == 1) then
+  if (game.cur_level == 1) then
     level1.mousepressed(x,y,button)
+  elseif (game.cur_level == 2) then
+    level2.mousepressed(x,y,button)
+  elseif (game.cur_level == 3) then
+    level3.mousepressed(x,y,button)
+  elseif (game.cur_level == "done") then
+    credits.mousepressed(x,y,button)
   end
 end
 
 function game.mousereleased(x,y,button)
-  if (cur_level == 1) then
+  if (game.cur_level == 1) then
     level1.mousereleased(x,y,button)
+  elseif (game.cur_level == 2) then
+    level2.mousereleased(x,y,button)
+  elseif (game.cur_level == 3) then
+    level3.mousereleased(x,y,button)
+  elseif (game.cur_level == "done") then
+    credits.mousereleased(x,y,button)
   end
 end
