52ce9291c3b10e2c5c05670b1e2208370fb1cd4b
Peter R Stanley <peterrstanley@gmail.com>
2014-04-27 20:44:50 -0400

    Completed game level.

--- src/game/level1/level1.lua
+++ src/game/level1/level1.lua
@@ -3,16 +3,15 @@ level1.assets = {
   background = love.graphics.newImage("game/level1/assets/level1.png")
 }
 local ch = ""
+local machine1 = machine.new(100,450)
 
 function level1.load()
-   machine1 = machine.new(100,450)
   machine1:load()
 end
 
 function level1.draw()
   love.graphics.draw(level1.assets.background,0,0)
-  love.graphics.print("level 1 "..game.user:getRepairValue(),50,400)
-  love.graphics.print(ch,100,400)
+  love.graphics.print("level 1 ",50,325)
   machine1:draw()
   game.user:draw()
 end
@@ -30,10 +29,10 @@ end
 function level1.keyreleased(key,unicode)
   machine1:keyreleased(key,unicode)
   game.user:keyreleased(key,unicode)
-  if (key == " " and machine1:inRange(game.user.x)) then
-    machine:increaseHealth(1)
+
+  if(machine1.health >= 100) then
+    game.cur_level = 2
   end
-  ch = key
 end
 
 function level1.mousepressed(x,y,button)
